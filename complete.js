04c93bc9328443714d55c45793db9a912c1c3268
Lilly Chen <chenlilly48@gmail.com>
2023-03-21 14:52:16 -0400

    add transcription + chatgpt

--- complete.js
+++ complete.js
@@ -4,9 +4,56 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeButton = document.getElementById('close');
   const review = document.getElementById('review');
   const status = document.getElementById('status');
+  const doctorSpeak = document.getElementById('doctorSpeak')
+  const transcriptionResult = document.getElementById('transcriptionResult')
+  const OPENAI_API_KEY = process.env.OPENAI_API_KEY
   let format;
   let audioURL;
   let encoding = false;
+  async function getTranscription(file) {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("model", "whisper-1");
+
+    try {
+      const response = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${OPENAI_API_KEY}`,
+        },
+        body: formData,
+      });
+
+      const data = await response.json();
+      console.log(data.text)
+      transcriptionResult.innerHTML = `Transcription: ${data.text}`;
+      return data.text
+    } catch (error) {
+      console.error("Error fetching transcription:", error);
+    }
+  }
+  async function getDoctorSpeak(translation) {
+    const prompt = `The following is a transcription of a conversation between a physician and a patient. Document the facts of the case in appropriate medical terms. Do not add additional notes that were not explicitly discussed: \n${translation}`;
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        messages: [{'role': 'user', 'content': `${prompt}`}],
+        model: 'gpt-4',
+        temperature: 0,
+        max_tokens: 200
+      }),
+    });
+    const data = await response.json();
+    const medicalDocumentation = data.choices[0].message.content.trim()
+    doctorSpeak.innerHTML = `Doctor Speak: ${medicalDocumentation}`;
+    return medicalDocumentation
+  }
+
+
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if(request.type === "createTab") {
       format = request.format;
@@ -46,6 +93,19 @@ document.addEventListener('DOMContentLoaded', () => {
         chrome.downloads.download({url: url, filename: `${currentDate}.${format}`, saveAs: true});
       };
       saveButton.style.display = "inline-block";
+      saveButton.onclick = async () => {
+        // Convert the audioURL to a File object
+        const response = await fetch(url);
+        const blob = await response.blob();
+        const file = new File([blob], `${currentDate}.${format}`, { type: blob.type });
+  
+        // Call the API and display the transcription result
+        transcription = getTranscription(file);
+        getDoctorSpeak(transcription)
+  
+        // Download the file
+        chrome.downloads.download({ url: url, filename: `${currentDate}.${format}`, saveAs: true });
+      };
     }
   });
   review.onclick = () => {
